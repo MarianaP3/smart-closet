fe548cb6dce7c42fe20d03e449b27cfdbf7ee25c
Mariana Pérez <marianasps003@gmail.com>
2026-05-29 10:47:05 -0600

    login page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { NewCountryPageComponent } from './pages/new-country-page/new-country-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomePageComponent},
+    { path: 'login', component: LoginPageComponent},
     { path: 'about', component: AboutPageComponent},
     { path: 'contact', component: ContactPageComponent},
     { path: '', redirectTo: '/home', pathMatch: 'full' },
