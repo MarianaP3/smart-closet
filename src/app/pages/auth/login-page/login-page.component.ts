052c8db0ce61ec318b33841423bc069b35a2cb34
Mariana Pérez <marianasps003@gmail.com>
2026-06-01 15:17:22 -0600

    authServiceAdded

--- src/app/pages/auth/login-page/login-page.component.ts
+++ src/app/pages/auth/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -7,4 +8,60 @@ import { RouterLink } from '@angular/router';
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css',
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  username: string = '';
+  password: string = '';
+  errorMessage: string | null = null;
+
+  newUsername: string = '';
+  newPassword: string = '';
+  registrationError: string = '';
+
+  onSubmit() {
+    if (this.username && this.password) {
+      this.authService.login(this.username, this.password).subscribe({
+        next: (response) => {
+          console.log('Login exitoso:', response);
+          localStorage.setItem('token', response.token); 
+          localStorage.setItem('username', this.username);
+          this.errorMessage = null; 
+          this.router.navigate(['/home']);
+        },
+        error: (error) => {
+          console.error('Error en login:', error);
+          this.errorMessage = error.error.msg || 'Error al iniciar sesión';
+        },
+      });
+    } else {
+      this.errorMessage = 'Por favor, completa todos los campos.';
+    }
+  }
+
+  onRegister() {
+    console.log('Registro con:', this.newUsername, this.newPassword);
+  
+    if (!this.newUsername || !this.newPassword) {
+      this.registrationError = 'Por favor, complete todos los campos';
+      return;
+    }
+
+    this.authService.register(this.newUsername, this.newPassword).subscribe(
+      response => {
+        console.log('Usuario registrado con éxito:', response);
+        this.registrationError = "Usuario Registrado con éxito";
+        this.router.navigate(['/login']);
+      },
+      error => {
+        console.error('Error al registrar el usuario:', error);
+        if (error.status === 400 && error.error.msg === 'Username ya existente') {
+          this.registrationError = 'El nombre de usuario ya está registrado. Intenta con otro.';
+        } else {
+          this.registrationError = 'Hubo un error al registrar el usuario. Intenta nuevamente.';
+        }
+      }
+    );
+  }
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,59 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { jwtDecode } from 'jwt-decode';
+
+interface JwtPayload {
+  username: string;
+  role: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:8080/api/auth'; 
+
+  constructor(private http: HttpClient, private router: Router) {}
+  getCurrentUser(): string | null {
+    return localStorage.getItem('username');
+  }
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { username, password });
+  }
+
+  register(username: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, { username, password });
+  }
+
+  logout() {
+    localStorage.removeItem('token');  
+    localStorage.removeItem('username');  
+    localStorage.removeItem('cart');  
+    this.router.navigate(['/home']).then(() => {
+      window.location.reload();
+    });
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  getRoleFromToken(): string | null {
+    const token = this.getToken();
+    if (token) {
+      try {
+        const decoded = jwtDecode<JwtPayload>(token);
+        return decoded.role || null;
+      } catch (error) {
+        console.error('Error al decodificar el token:', error);
+        return null;
+      }
+    }
+    return null;
+  }
+
+     
+}
