7a37c30617b6fa481acc7ff1bd0a1b43d5ede4b4
Mariana Pérez <marianasps003@gmail.com>
2026-06-01 15:18:35 -0600

    authServiceAdded

--- src/app/pages/auth/login-page/login-page.component.ts
+++ src/app/pages/auth/login-page/login-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { AuthService } from '../../../services/auth.service';
+import { AuthService } from '../../../../app/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
